3853dd00d2993da3c451b65c6df074140de2419d
screm1n <screminoctavio@gmail.com>
2023-11-11 17:48:24 -0300

    age converter done

---
+++ exercises/14.2 JavaScript Numbers/dog_age_converter.js
@@ -0,0 +1,6 @@
+// This is a dog age to human age converter
+const dogAge = 5;
+const humanAge = (dogAge - 2) * 4 + 21;
+
+
+console.log(`The human equivalent age for a ${dogAge}-year-old dog is approximately ${humanAge} years.`)
