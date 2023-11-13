1b92805b62bab8186b289e0716c872e57b5fb87a
screm1n <screminoctavio@gmail.com>
2023-11-13 19:42:40 -0300

    leap year challenge done

---
+++ exercises/15.1 Conditionals/leap_year_checker.js
@@ -0,0 +1,9 @@
+function leapYearChecker(year) {
+    if (year % 4 == 0 && (year % 100 !== 0 || year % 400 == 0)) {
+        return year + ' is a Leap yer.';
+    } else {
+        return year + ' is not a leap year.';
+    }
+}
+
+console.log(leapYearChecker(2023));
