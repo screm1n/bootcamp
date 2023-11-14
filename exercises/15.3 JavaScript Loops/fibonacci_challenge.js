01ebefd270e49be3569a88344eb3eaffa30b0c59
screm1n <screminoctavio@gmail.com>
2023-11-14 18:22:27 -0300

    fibonacci challenge done

---
+++ exercises/15.3 JavaScript Loops/fibonacci_challenge.js
@@ -0,0 +1,21 @@
+function fibonacciGenerator(n) {
+    
+    let output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1];
+    } 
+    else {
+        output = [0, 1];
+
+        for (let i = 2; i < n; i++) { 
+        output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output;
+}
+
+output = fibonacciGenerator(50);
+console.log(output);
