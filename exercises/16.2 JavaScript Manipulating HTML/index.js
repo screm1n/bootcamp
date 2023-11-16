4f66ea0d8342d3bf341a902f783c0f1041756c38
screm1n <screminoctavio@gmail.com>
2023-11-16 13:52:51 -0300

    module concluded

--- exercises/16.2 JavaScript Manipulating HTML/index.js
+++ exercises/16.2 JavaScript Manipulating HTML/index.js
@@ -1,7 +1,1 @@
-document.getElementById('third').innerHTML = 'finalmente consegui vsf';
-
-document.querySelector("a").style.color = "red";
-
-document.querySelector("h1").classList.add("huge")
-
-document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
