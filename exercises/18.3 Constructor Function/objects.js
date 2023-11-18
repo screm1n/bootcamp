1a244fed7ae0f15165d38495f2f27e8100905feb
screm1n <screminoctavio@gmail.com>
2023-11-18 16:02:26 -0300

    project done

--- exercises/18.3 Constructor Function/objects.js
+++ exercises/18.3 Constructor Function/objects.js
@@ -3,7 +3,12 @@ function HouseKeeper (name, experience, repertorie) {
     this.name = name; 
     this.experience = experience; 
     this.repertorie = repertorie; 
+    this.clean = function () {
+        return "Cleaning in progress..."
+    }
 }
 
 var houseKeeper1 = new HouseKeeper ("Rachel", 4, ["lobby", "room"]);
-console.log(houseKeeper1);
+console.log(houseKeeper1.clean());
+
+
