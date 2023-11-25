39ad4042a32406a2f12b968977ad1ca7380bfb8b
screm1n <screminoctavio@gmail.com>
2023-11-25 18:57:46 -0300

    http request

--- exercises/Backend/3.1 Express Server/index.js
+++ exercises/Backend/3.1 Express Server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 const app = express();
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}.`);
-}); 
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3856, () => {
+  console.log("Server running on port 3856.");
+});
--- exercises/Backend/3.2 HTTP Requests/index.js
+++ exercises/Backend/3.2 HTTP Requests/index.js
@@ -1,19 +1,16 @@
 import express from "express";
 const app = express();
-const port = 3000;
+
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send(`
+  <h1>Welcome to my website</h1>
+  <p>This site was made with Node.js, Express and npm.</p>
+  <h2>Contact Me</h2>
+  <p><a href="https://github.com/screm1n">My GitHub</a></p>
+  `);
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(5932, () => {
+  console.log("Server running on port 5932.");
 });
