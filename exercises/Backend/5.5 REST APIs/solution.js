88b2ef07aca88513ed7ff24b28d7f9621b7afa0d
Octávio <screminoctavio@gmail.com>
2024-01-08 23:15:38 -0300

    Update solution.js

--- exercises/Backend/5.5 REST APIs/solution.js
+++ exercises/Backend/5.5 REST APIs/solution.js
@@ -6,7 +6,6 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
-//Add your own bearer token from the previous lesson.
 const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
