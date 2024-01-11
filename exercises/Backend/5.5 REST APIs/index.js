5463122370f8304c5b25611ec1c0f454b0c11ec5
Octávio <screminoctavio@gmail.com>
2024-01-10 23:23:29 -0300

    cleaning index

--- exercises/Backend/5.5 REST APIs/index.js
+++ exercises/Backend/5.5 REST APIs/index.js
@@ -6,8 +6,6 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
-// HINTs: Use the axios documentation as well as the video lesson to help you.
-// https://axios-http.com/docs/post_example
 // Use the Secrets API documentation to figure out what each route expects and how to work with it.
 // https://secrets-api.appbrewery.com/
 
