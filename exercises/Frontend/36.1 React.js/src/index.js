8953e5be75cece05cc9b225bff50576c1da049b5
Octávio <screminoctavio@gmail.com>
2024-02-09 08:56:03 -0300

    react section starting files

---
+++ exercises/Frontend/36.1 React.js/src/index.js
@@ -0,0 +1,7 @@
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
