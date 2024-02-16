a01b9c280ebc5774483fc24e11db8deb30e8f18f
Octávio <screminoctavio@gmail.com>
2024-02-15 23:28:26 -0300

    uploading the exercises done

---
+++ exercises/Frontend/36.1 React/JSX Attributes and Styling/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
