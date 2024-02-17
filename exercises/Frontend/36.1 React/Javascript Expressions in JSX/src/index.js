26ce3d47928348b729b47eca73d33ce10729d7eb
Octávio <screminoctavio@gmail.com>
2024-02-16 23:36:34 -0300

    jsx exercises files updating

---
+++ exercises/Frontend/36.1 React/Javascript Expressions in JSX/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Angela";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
