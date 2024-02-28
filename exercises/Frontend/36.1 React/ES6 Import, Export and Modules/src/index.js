e664434ba1c9e8f928052007e99ba44184bef66e
Octávio <screminoctavio@gmail.com>
2024-02-27 23:34:41 -0300

    es6 import/export/modules files updating

---
+++ exercises/Frontend/36.1 React/ES6 Import, Export and Modules/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
+  document.getElementById("root")
+);
